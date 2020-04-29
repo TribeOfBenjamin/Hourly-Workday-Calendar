fbd430a36ffa69c8d5752bfd14624503c165de03
TribeOfBenjamin <benjamin.vasko.33@gmail.com>
2020-04-29 15:15:18 -0500

    added moment.js script and today's date in the header

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,28 @@
 
 $(document).ready(function(){
 
+    let today = moment();
+
+    let todaysDate = $("<p>").text(today.format("dddd, MMMM Do, YYYY"));
+    $("#todaysDate").append(todaysDate);
+    $("#todaysDate").css("font-weight", "bolder");
+
 
     $( "button" ).on( "click", function() {
         
         alert( "hello!" );
       });
+
+
+
+
+
+
+
+
+
+
+
+
 
 })
