8f0f3bce569e68f124f63903d77b9c16d62f0c56
TribeOfBenjamin <benjamin.vasko.33@gmail.com>
2020-04-30 09:12:47 -0500

    added if/then statement checking the current hour and changing text color if it matches

--- assets/script.js
+++ assets/script.js
@@ -11,22 +11,13 @@ let todaysDate = $("<p>").text(today.format("dddd, MMMM Do, YYYY"));
 $("#todaysDate").append(todaysDate);
 $("#todaysDate").css("font-weight", "bolder");
 
-
-// Trying to create a save function
-
-
-
-
-// Gets text from local storage and displays in rows (first three for now)
+// Gets text from local storage and displays in rows (first 3 for now)
 for (let i = 8; i < 11; i++) {
 
     $( "#hour" + i ).val(localStorage.getItem( i + ":00" ));
 }
 
-
-
-
-// On click event for save button
+// On click event for save buttons (first 3 for now; maybe turn into a function?)
 $( ".saveButton8" ).on( "click", function() {
         
     localStorage.setItem("8:00", hour8Text.val());
@@ -49,7 +40,17 @@ $( ".saveButton10" ).on( "click", function() {
     });
 
 
+// Checks if the current hour matches and changes text red
+if (moment().get("hour") === 8) {
+    $("#hour8").css("color", "red");
+} else if (moment().get("hour") === 9) {
+    $("#hour9").css("color", "red");
+} else if (moment().get("hour") === 10) {
+    $("#hour10").css("color", "red");
+}
 
+
+console.log(moment().get("hour"));
 
 
 
